07322afadbb7fc2d2546c6bd4de9e0e0750045a4
Bipul <Bipulkumarbk@gmail.com>
2019-07-19 18:39:43 +0530

    first Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import{FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 
-const routes: Routes = [];
+import {AboutusComponent} from '../app/aboutus/aboutus.component';
+import {ContactusComponent} from '../app/contactus/contactus.component';
+import {HomeComponent} from '../app/home/home.component';
+
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'contactus',component:ContactusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
